388912454e055d24fccee181299da9370f514c3b
shashankholla <shashu1999@gmail.com>
2017-10-15 19:32:44 +0530

    Add files via upload

---
+++ json.php
@@ -0,0 +1,64 @@
+{
+  "android": [
+    {
+      "book": "Book 1",
+      "author": "SP_SLI_1.1_A01_UG",
+      "link": "http://linux.dell.com/files/DSP/DSP-1.1-Live/DSP_SLI_1.1_A01_UG.pdf"
+    },
+    {
+      "book": "Book 2",
+      "author": "Release Notes",
+      "link": "http://linux.dell.com/files/DSP/DSP-1.1-Live/ReleaseNotes.txt"
+    },
+    {
+      "book": "2.0 - 2.1",
+      "author": "Eclair",
+      "link": "link level 5 - 7"
+    },
+    {
+      "book": "2.2",
+      "author": "Froyo",
+      "link": "link level 8"
+    },
+    {
+      "book": "2.3",
+      "author": "Gingerbread",
+      "link": "link level 9 - 10"
+    },
+    {
+      "book": "3.0 - 3.2",
+      "author": "Honeycomb",
+      "link": "link level 11 - 13"
+    },
+    {
+      "book": "4.0",
+      "author": "Ice Cream Sandwich",
+      "link": "link level 14 - 15"
+    },
+    {
+      "book": "4.1 - 4.3",
+      "author": "JellyBean",
+      "link": "link level 16 - 18"
+    },
+    {
+      "book": "4.4",
+      "author": "KitKat",
+      "link": "link level 19"
+    },
+    {
+      "book": "5.0 - 5.1",
+      "author": "Lollipop",
+      "link": "link level 21 - 22"
+    },
+    {
+      "book": "6.0",
+      "author": "Marshmallow",
+      "link": "link level 23"
+    },
+    {
+      "book": "7.0 - 7.1",
+      "author": "Nougat",
+      "link": "link level 24 - 25"
+    }
+  ]
+}
