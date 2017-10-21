dc1264260b520bfb88b989e76b6ef1312c80a917
shashankholla <shashu1999@gmail.com>
2017-10-21 14:16:10 +0530

    Update json.php

--- json.php
+++ json.php
@@ -4,73 +4,85 @@
       "book": "Book 1",
       "author": "SP_SLI_1.1_A01_UG",
       "link": "http://linux.dell.com/files/DSP/DSP-1.1-Live/DSP_SLI_1.1_A01_UG.pdf",
-      "img":"https://i.imgur.com/F0bqnH9.jpg"
+      "img":"https://i.imgur.com/F0bqnH9.jpg",
+      "filename":"DSP-1.1-Live/DSP_SLI_1.1_A01_UG.pdf"
     },
     {
       "book": "Book 2",
       "author": "Release Notes",
       "link": "http://che.org.il/wp-content/uploads/2016/12/pdf-sample.pdf",
-"img":"https://i.imgur.com/J1gMWeP.jpg"
+"img":"https://i.imgur.com/J1gMWeP.jpg",
+"filename":"pdf-sample.pdf"
     },
     {
       "book": "2.0 - 2.1",
       "author": "Sample 3",
       "link": "www.africau.edu/images/default/sample.pdf",
-"img":"https://i.imgur.com/J1gMWeP.jpg"
+"img":"https://i.imgur.com/J1gMWeP.jpg",
+"filename":""
     },
     {
       "book": "2.2",
       "author": "Froyo",
       "link": "link level 8",
-"img":"https://i.imgur.com/J1gMWeP.jpg"
+"img":"https://i.imgur.com/J1gMWeP.jpg",
+"filename":""
     },
     {
       "book": "2.3",
       "author": "Gingerbread",
       "link": "link level 9 - 10",
-"img":"https://i.imgur.com/J1gMWeP.jpg"
+"img":"https://i.imgur.com/J1gMWeP.jpg",
+"filename":""
     },
     {
       "book": "3.0 - 3.2",
       "author": "Honeycomb",
       "link": "link level 11 - 13",
-"img":"https://i.imgur.com/J1gMWeP.jpg"
+"img":"https://i.imgur.com/J1gMWeP.jpg",
+"filename":""
     },
     {
       "book": "4.0",
       "author": "Ice Cream Sandwich",
       "link": "link level 14 - 15",
-"img":"https://i.imgur.com/J1gMWeP.jpg"
+"img":"https://i.imgur.com/J1gMWeP.jpg",
+"filename":""
     },
     {
       "book": "4.1 - 4.3",
       "author": "JellyBean",
       "link": "link level 16 - 18",
-"img":"https://i.imgur.com/J1gMWeP.jpg"
+"img":"https://i.imgur.com/J1gMWeP.jpg",
+"filename":""
     },
     {
       "book": "4.4",
       "author": "KitKat",
       "link": "link level 19",
-"img":"https://i.imgur.com/J1gMWeP.jpg"
+"img":"https://i.imgur.com/J1gMWeP.jpg",
+"filename":""
     },
     {
       "book": "5.0 - 5.1",
       "author": "Lollipop",
       "link": "link level 21 - 22",
-"img":"https://i.imgur.com/J1gMWeP.jpg"
+"img":"https://i.imgur.com/J1gMWeP.jpg",
+"filename":""
     },
     {
       "book": "6.0",
       "author": "Marshmallow",
       "link": "link level 23",
-"img":"https://i.imgur.com/J1gMWeP.jpg"
+"img":"https://i.imgur.com/J1gMWeP.jpg",
+"filename":""
     },
     {
       "book": "7.0 - 7.1",
       "author": "Nougat",
       "link": "link level 24 - 25",
-"img":"https://i.imgur.com/J1gMWeP.jpg"
+"img":"https://i.imgur.com/J1gMWeP.jpg",
+"filename":""
     }
   ]
 }
