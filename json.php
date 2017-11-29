1eb8c975ba686f81b5f75faf2359fd954543fb13
shashankholla <shashu1999@gmail.com>
2017-11-29 20:47:05 +0530

    Update json.php

--- json.php
+++ json.php
@@ -1,120 +1,233 @@
 {
   "android": [
     {
-      "book": "Upper limb",
-      "author": "handouts",
+      "book": "Complete Upper limb",
+      "author": "Upper limb",
       "link": "https://github.com/shashankholla/upper-limb/raw/master/1.BONES%20OF%20UPPER%20LIMB%20(1).pdf",
       "img":"https://i.imgur.com/F0bqnH9.jpg",
       "filename":"1.BONES OF UPPER LIMB (1).pdf"
     },
     {
       "book": "Pectoral region",
-      "author": "handouts",
+      "author": "Upper limb",
       "link": "https://github.com/shashankholla/upper-limb/raw/master/2.PECTORAL%20REGION.pdf",
 "img":"https://i.imgur.com/J1gMWeP.jpg",
 "filename":"2.PECTORAL REGION.pdf"
     },
     {
       "book": "Axilla",
-      "author": "handouts",
+      "author": "Upper limb",
       "link": "https://github.com/shashankholla/upper-limb/raw/master/3.AXILLA.pdf",
 "img":"https://i.imgur.com/J1gMWeP.jpg",
 "filename":"3.AXILLA.pdf"
     },
     {
       "book": "Scapular region",
-      "author": "handouts",
+      "author": "Upper limb",
       "link": "https://github.com/shashankholla/upper-limb/raw/master/4.SCAPULAR%20REGION%20.pdf",
 "img":"https://i.imgur.com/J1gMWeP.jpg",
 "filename":"4.SCAPULAR REGION .pdf"
     },
     {
       "book": "Arm",
-      "author": "handouts",
+      "author": "Upper limb",
       "link": "https://github.com/shashankholla/upper-limb/raw/master/5.ARM.pdf",
 "img":"https://i.imgur.com/J1gMWeP.jpg",
 "filename":"5.ARM.pdf"
     },
     {
       "book": "Forearm and hand",
-      "author": "handouts",
+      "author": "Upper limb",
       "link": "https://github.com/shashankholla/upper-limb/raw/master/6.FOREARM%20%26%20HAND.pdf",
 "img":"https://i.imgur.com/J1gMWeP.jpg",
 "filename":"6.FOREARM & HAND.pdf"
     },
     {
       "book": "Joints of upper limb",
-      "author": "handouts",
+      "author": "Upper limb",
       "link": "https://github.com/shashankholla/upper-limb/raw/master/7.JOINTS%20OF%20UPPER%20LIMB.pdf",
 "img":"https://i.imgur.com/J1gMWeP.jpg",
 "filename":"7.JOINTS OF UPPER LIMB.pdf"
     },
     {
       "book": "Bones of lower limb",
-      "author": "handouts",
+      "author": "Lower limb",
       "link": "https://github.com/shashankholla/upper-limb/raw/master/1.BONES%20OF%20LOWER%20LIMB.pdf",
 "img":"https://i.imgur.com/J1gMWeP.jpg",
 "filename":"1.BONES OF LOWER LIMB.pdf"
     },
     {
       "book": "Front of thigh",
-      "author": "handouts",
+      "author": "Lower limb",
       "link": "https://github.com/shashankholla/upper-limb/raw/master/2.FRONT%20OF%20THIGH.pdf",
 "img":"https://i.imgur.com/J1gMWeP.jpg",
 "filename":"2.FRONT OF THIGH.pdf"
     },
     {
       "book": "Medial side of thigh",
-      "author": "handouts",
+      "author": "Lower limb",
       "link": "https://github.com/shashankholla/upper-limb/raw/master/3.MEDIAL%20SIDE%20OF%20THIGH.pdf",
 "img":"https://i.imgur.com/J1gMWeP.jpg",
 "filename":"3.MEDIAL SIDE OF THIGH.pdf"
     },
     {
       "book": "Gluteal region",
-      "author": "handouts",
+      "author": "Lower limb",
       "link": "https://github.com/shashankholla/upper-limb/raw/master/4.GLUTEAL%20REGION.pdf",
 "img":"https://i.imgur.com/J1gMWeP.jpg",
 "filename":"4.GLUTEAL REGION.pdf"
     },
     {
       "book": "Popliteal fossa",
-      "author": "handouts",
+      "author": "Lower limb",
       "link": "https://github.com/shashankholla/upper-limb/raw/master/5.POPLITEAL%20FOSSA.pdf",
 "img":"https://i.imgur.com/J1gMWeP.jpg",
 "filename":"5.POPLITEAL FOSSA.pdf"
     },
  {
       "book": "Back of thigh",
-      "author": "handouts",
+      "author": "Lower limb",
       "link": "https://github.com/shashankholla/upper-limb/raw/master/6.BACK_OF_THIGH.pdf",
 "img":"https://i.imgur.com/J1gMWeP.jpg",
 "filename":"6.BACK_OF_THIGH.pdf"
     },
 {
-      "book": "Anterolateral,leg",
-      "author": "handouts",
+      "book": "Anterolateral leg",
+      "author": "Lower limb",
       "link": "https://github.com/shashankholla/upper-limb/raw/master/7._FRONT_LATERAL_AND_MEDIAL_SIDES_OF_LEG___DORSUM_OF_FOOT.pdf",
 "img":"https://i.imgur.com/J1gMWeP.jpg", 
 "filename":"7._FRONT_LATERAL_AND_MEDIAL_SIDES_OF_LEG___DORSUM_OF_FOOT.pdf"
     },
 {
       "book": "Back of leg",
-      "author": "handouts",
+      "author": "Lower limb",
       "link": "https://github.com/shashankholla/upper-limb/raw/master/8.BACK_OF_LEG.pdf",
 "img":"https://i.imgur.com/J1gMWeP.jpg",
 "filename":"8.BACK_OF_LEG.pdf"
     },
 {
-      "book": "Venous and lymphatic drainage of lower limb",
-      "author": "handouts",
+      "book": "Venous,lymphatic drainage",
+      "author": "Lower limb",
       "link": "https://github.com/shashankholla/upper-limb/raw/master/9.VENOUS___LYMPHATIC_DRAINAGE_OF_LOWRRE_LIMB.pdf",
 "img":"https://i.imgur.com/J1gMWeP.jpg",
 "filename":"9.VENOUS___LYMPHATIC_DRAINAGE_OF_LOWRRE_LIMB.pdf"
     },
+
+{
+      "book": "Osteology Head,Neck",
+      "author": "Head and Neck",
+      "link": "https://github.com/shashankholla/upper-limb/raw/master/1.OSTEOLOGY%20OF%20HEAD%20AND%20NECK.pdf",
+      "img":"https://i.imgur.com/F0bqnH9.jpg",
+      "filename":"1.OSTEOLOGY OF HEAD AND NECK.pdf"
+    },
+    {
+      "book": "Scalp,Temple,Face",
+      "author": "Head and Neck",
+      "link": "https://github.com/shashankholla/upper-limb/raw/master/2.SCALP%2CFACE%2CLACRIMAL%20APPARATUS.pdf",
+"img":"https://i.imgur.com/J1gMWeP.jpg",
+"filename":"2.SCALP,FACE,LACRIMAL APPARATUS.pdf"
+    },
+    {
+      "book": "Side of neck",
+      "author": "Head and Neck",
+      "link": "https://github.com/shashankholla/upper-limb/raw/master/3.SIDE%20OF%20NECK.pdf",
+"img":"https://i.imgur.com/J1gMWeP.jpg",
+"filename":"3.SIDE OF NECK.pdf"
+    },
+    {
+      "book": "Anterior traingle",
+      "author": "Head and Neck",
+      "link": "https://github.com/shashankholla/upper-limb/raw/master/4.ANTERIOR%20TRIANGLE%20modified.pdf",
+"img":"https://i.imgur.com/J1gMWeP.jpg",
+"filename":"4.ANTERIOR TRIANGLE modified.pdf"
+    },
+  {
+      "book": "Partoid region",
+      "author": "Head and Neck",
+      "link": "https://github.com/shashankholla/upper-limb/raw/master/5.PAROTID%20REGION.pdf",
+"img":"https://i.imgur.com/J1gMWeP.jpg",
+"filename":"5.PAROTID REGION.pdf"
+    },
+    {
+      "book": "Temporal & Infratemporal",
+      "author": "Head and Neck",
+      "link": "https://github.com/shashankholla/upper-limb/raw/master/6.TEMPORAL%20%26%20INFRATEMPORAL%20REGIONS.pdf",
+"img":"https://i.imgur.com/J1gMWeP.jpg",
+"filename":"6.TEMPORAL & INFRATEMPORAL REGIONS.pdf"
+    },
+{
+      "book": "Structures in neck",
+      "author": "Head and Neck",
+      "link": "https://github.com/shashankholla/upper-limb/raw/master/7.STRUCTURES%20IN%20THE%20NECK.pdf",
+"img":"https://i.imgur.com/J1gMWeP.jpg",
+"filename":"7.STRUCTURES IN THE NECK.pdf"
+    },
+    {
+      "book": "Back of neck",
+      "author": "Head and Neck",
+      "link": "https://github.com/shashankholla/upper-limb/raw/master/8.BACK%20OF%20NECK.pdf",
+"img":"https://i.imgur.com/J1gMWeP.jpg",
+"filename":"8.BACK OF NECK.pdf"
+    },
+    {
+      "book": "Contenets of vertebral column",
+      "author": "Head and Neck",
+      "link": "https://github.com/shashankholla/upper-limb/raw/master/9.%20CONTENTS%20OF%20VERTEBRAL%20CANAL.pdf",
+"img":"https://i.imgur.com/J1gMWeP.jpg",
+"filename":"9. CONTENTS OF VERTEBRAL CANAL.pdf"
+    },
+    {
+      "book": "Cranial cavity",
+      "author": "Head and Neck",
+      "link": "https://github.com/shashankholla/upper-limb/raw/master/10.CRANIAL%20CAVITY.pdf",
+"img":"https://i.imgur.com/J1gMWeP.jpg",
+"filename":"10.CRANIAL CAVITY.pdf"
+    },
+    {
+      "book": "Contenets of orbit",
+      "author": "Head and Neck",
+      "link": "https://github.com/shashankholla/upper-limb/raw/master/11.CONTENTS%20OF%20ORBIT.pdf",
+"img":"https://i.imgur.com/J1gMWeP.jpg",
+"filename":"11.CONTENTS OF ORBIT.pdf"
+    },
+    {
+      "book": "Mouth and pharynx",
+      "author": "Head and Neck",
+      "link": "https://github.com/shashankholla/upper-limb/raw/master/12.MOUTH%20%26%20PHARYNX.pdf",
+"img":"https://i.imgur.com/J1gMWeP.jpg",
+"filename":"12.MOUTH & PHARYNX.pdf"
+    },
+ {
+      "book": "Nose & paranasal sinuses",
+      "author": "Head and Neck",
+      "link": "https://github.com/shashankholla/upper-limb/raw/master/13.NOSE%20%26%20PARANASAL%20SINUSES.pdf",
+"img":"https://i.imgur.com/J1gMWeP.jpg",
+"filename":"13.NOSE & PARANASAL SINUSES.pdf"
+    },
+{
+      "book": "Larynx",
+      "author": "Head and Neck",
+      "link": "https://github.com/shashankholla/upper-limb/raw/master/14.LARYNX.pdf",
+"img":"https://i.imgur.com/J1gMWeP.jpg", 
+"filename":"14.LARYNX.pdf"
+    },
+{
+      "book": "Tongue",
+      "author": "Head and Neck",
+      "link": "https://github.com/shashankholla/upper-limb/raw/master/15.TONGUE.pdf",
+"img":"https://i.imgur.com/J1gMWeP.jpg",
+"filename":"15.TONGUE.pdf"
+    },
+{
+      "book": "Ear",
+      "author": "Head and Neck",
+      "link": "https://github.com/shashankholla/upper-limb/raw/master/16.EAR.pdf",
+"img":"https://i.imgur.com/J1gMWeP.jpg",
+"filename":"16.EAR.pdf"
+},
 {
       "book": "Blank doc",
-      "author": "handouts",
+      "author": "Blank",
       "link": "",
 "img":"https://i.imgur.com/J1gMWeP.jpg",
 "filename":"Blank doc.pdf"
