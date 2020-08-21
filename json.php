dfc21a5479c3329f89ce44fc00e5205b4ac21f8e
avinashrcl <33034598+avinashrcl@users.noreply.github.com>
2020-08-21 15:04:10 +0530

    Update json.php

--- json.php
+++ json.php
@@ -492,7 +492,7 @@
 {
  "book": "Install new medo",
 "author": "Anatomy",
-          "link": "https://github.com/shashankholla/mednotesjson/raw/master/newone.pdf",
+          "link": "https://github.com/shashankholla/mednotesjson/raw/master/meds.pdf",
 "img":"https://i.imgur.com/F0bqnH9.jpg",
  "filename":"Install new medo"
 
