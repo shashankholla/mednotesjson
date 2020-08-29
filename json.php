9e3da6e3a875df95010e97d203fd250262bfb583
avinashrcl <33034598+avinashrcl@users.noreply.github.com>
2020-08-29 20:52:37 +0530

    Update json.php

--- json.php
+++ json.php
@@ -3,7 +3,7 @@
    
 {
  "book": "Install new medo",
-"author": "Search for pub:SHARCodes in play store",
+"author": "Search for Medo in play store",
           "link": "https://github.com/shashankholla/mednotesjson/raw/master/meds.pdf",
 "img":"https://i.imgur.com/F0bqnH9.jpg",
  "filename":"meds.pdf"
